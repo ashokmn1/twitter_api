08bed0c72455e05df32a63831c53f0b47b2c7499
Ashok M N <ashok.n.19@mountblue.tech>
2022-04-18 08:20:53 +0530

    connection updates

--- dbconnection.js
+++ dbconnection.js
@@ -1,11 +1,15 @@
 const {Pool} = require('pg')
 
 const client = new Pool ({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'Ashokmn@123',
-    port: 5432,
+    // user: 'postgres',
+    // host: 'localhost',
+    // database: 'postgres',
+    // password: 'Ashokmn@123',
+    // port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
 })
 
 module.exports = client;
