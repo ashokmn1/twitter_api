34b8fd32a34e19517d95ebc317997ca975d96b6a
Ashok M N <ashok.n.19@mountblue.tech>
2022-04-22 07:16:45 +0530

    cors fix

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express()
 app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
-  // const corsWhiteList = ['http://localhost:3000', "https://node-api-twitter.herokuapp.com/"]
+  const corsWhiteList = ['http://localhost:3000', "https://node-api-twitter.herokuapp.com/"]
 
-  // if (corsWhiteList.indexOf(req.headers.origin) !== -1) {
+  if (corsWhiteList.indexOf(req.headers.origin) !== -1) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://node-api-twitter.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // }
+  }
   // Pass to next layer of middleware
   next();
 });
