fecbe9ceafe865b53a57cda14623c4c065014e88
Ashok M N <ashok.n.19@mountblue.tech>
2022-04-22 12:00:32 +0530

    cors fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(function (req, res, next) {
   // if (corsWhiteList.indexOf(req.headers.origin) !== -1) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000' );
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000' );
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
